6361f2bb2b501f5849613a1d85e4daf437b6358b
Darren Tham <darrent9859@gmail.com>
2022-07-24 16:15:31 -0400

    Updated colors

--- src/Colors.js
+++ src/Colors.js
@@ -1,14 +1,14 @@
 const Colors = {
-	RED: 'rgb(255, 158, 158)',
-	ORANGE: 'rgb(255, 219, 148)',
-	YELLOW: 'rgb(251, 255, 181)',
-	GREEN: 'rgb(186, 255, 184)',
-	BLUE: 'rgb(173, 222, 255)',
-	PURPLE: 'rgb(214, 189, 255)',
+	RED: 'rgb(255, 145, 145)',
+	ORANGE: 'rgb(255, 211, 125)',
+	YELLOW: 'rgb(250, 255, 161)',
+	GREEN: 'rgb(173, 255, 171)',
+	BLUE: 'rgb(166, 219, 255)',
+	PURPLE: 'rgb(208, 179, 255)',
 	WHITE: 'rgb(255, 255, 255)',
-	BLACK: 'rgb(38, 38, 38)',
+	BLACK: 'rgb(0, 0, 0)',
 	GRAY: 'rgb(121, 125, 128)',
-	LIGHT_GRAY: 'rgb(184, 188, 191)'
+	LIGHT_GRAY: 'rgb(187, 190, 196)'
 }
 
 export default Colors
