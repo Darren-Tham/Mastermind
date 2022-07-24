efc79c87abae5c2b283c0008647765b88ca519a7
Darren Tham <darrent9859@gmail.com>
2022-07-24 14:26:16 -0400

    Finished logics of the game

--- src/Colors.js
+++ src/Colors.js
@@ -1,15 +1,14 @@
 const Colors = {
-	RED: '#ff9e9e',
-	ORANGE: '#ffdb94',
-	YELLOW: '#fbffb5',
-	GREEN: '#baffb8',
-	BLUE: '#addeff',
-	PURPLE: '#d6bdff',
-	WHITE: '#f9f5ff',
-	BLACK: '#262626',
-	GRAY: '#797d80',
-	LIGHT_GRAY: '#989c9e',
-	LIGHTER_GRAY: '#b8bcbf'
+	RED: 'rgb(255, 158, 158)',
+	ORANGE: 'rgb(255, 219, 148)',
+	YELLOW: 'rgb(251, 255, 181)',
+	GREEN: 'rgb(186, 255, 184)',
+	BLUE: 'rgb(173, 222, 255)',
+	PURPLE: 'rgb(214, 189, 255)',
+	WHITE: 'rgb(255, 255, 255)',
+	BLACK: 'rgb(38, 38, 38)',
+	GRAY: 'rgb(121, 125, 128)',
+	LIGHT_GRAY: 'rgb(184, 188, 191)'
 }
 
 export default Colors
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,7 +5,7 @@ import ColorContainer from './ColorContainer'
 import MainBall from './MainBall'
 import CheckerBall from './CheckerBall'
 
-const { RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE, BLACK, GRAY, LIGHT_GRAY, LIGHTER_GRAY } = Colors
+const { RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE, BLACK, GRAY, LIGHT_GRAY } = Colors
 
 class Board extends Component {
   constructor(props) {
@@ -42,13 +42,13 @@ class Board extends Component {
           if (i === currIdx) {
             currBalls.push(this.createMainBall(j, GRAY, GRAY, this.handleMainClick, false, true))
           } else {
-            currBalls.push(this.createMainBall(j, LIGHTER_GRAY, LIGHTER_GRAY, null, false, false))
+            currBalls.push(this.createMainBall(j, LIGHT_GRAY, LIGHT_GRAY, null, false, false))
           }
         } else {
           if (i === currIdx) {
             currBalls.push(this.createCheckerBall(j, GRAY))
           } else {
-            currBalls.push(this.createCheckerBall(j, LIGHTER_GRAY))
+            currBalls.push(this.createCheckerBall(j, LIGHT_GRAY))
           }
         }
       }
@@ -66,7 +66,7 @@ class Board extends Component {
     for (let i = 0; i < codeLen; i++) {
       const randomIdx = Math.floor(Math.random() * colors.length)
       const randomColor = colors[randomIdx]
-      answerRow.push(this.createMainBall(i, LIGHTER_GRAY, randomColor, null, true, false))
+      answerRow.push(this.createMainBall(i, LIGHT_GRAY, randomColor, null, true, false))
 
       if (!allowDuplicates) {
         colors = colors.slice(0, randomIdx).concat(colors.slice(randomIdx + 1))
@@ -76,10 +76,82 @@ class Board extends Component {
     return answerRow
   }
 
+  updateRow = () => {    
+    const mainBalls = this.state.mainBalls.slice()
+    const currIdx = this.state.currIdx
+    const nextIdx = currIdx - 1
+    const currRow = mainBalls[currIdx]
+    const nextRow = []
+    const n = this.state.codeLen
+
+    for (let i = 0; i < n; i++) {
+      const color = currRow[i].props.color
+      currRow[i] = this.createMainBall(i, color, color, null, false, false)
+      nextRow.push(this.createMainBall(i, GRAY, GRAY, this.handleMainClick, false, true))
+    }
+
+    mainBalls[currIdx] = currRow
+    mainBalls[nextIdx] = nextRow
+
+    this.setState({ 
+      currIdx: nextIdx,
+      mainBalls,
+    })
+  }
+
+  updateChecker = () => {
+    const mainBalls = this.state.mainBalls.slice()
+    const checkerBalls = this.state.checkerBalls.slice()
+    const currIdx = this.state.currIdx
+    const nextIdx = currIdx - 1
+    const currRow = mainBalls[currIdx]
+    const answerRow = this.state.answerRow.slice()
+    const n = this.state.codeLen
+    const newChecker = []
+    const nextChecker = []
+
+    const currRowHashmap = {}
+    const answerRowHashmap = {}
+
+    let keyId = 0
+    for (let i = 0; i < n; i++) {
+      const currColor = currRow[i].props.mainColor
+      const answerColor = answerRow[i].props.mainColor
+      if (currColor === answerColor) {
+        newChecker.push(this.createCheckerBall(keyId++, RED))
+      } else {
+        currRowHashmap[currColor] = currRowHashmap[currColor] === undefined ? 1 : currRowHashmap[currColor] + 1
+        answerRowHashmap[answerColor] = answerRowHashmap[answerColor] === undefined ? 1 : answerRowHashmap[answerColor] + 1
+      }
+    }
+
+    for (const mapKey in currRowHashmap) {
+      if (answerRowHashmap[mapKey] !== undefined) {
+        const min = Math.min(currRowHashmap[mapKey], answerRowHashmap[mapKey])
+        for (let i = 0; i < min; i++) {
+          newChecker.push(this.createCheckerBall(keyId++, WHITE))
+        }
+      }
+    }
+
+    for (let i = newChecker.length; i < n; i++) {
+      newChecker.push(this.createCheckerBall(keyId++, GRAY))
+    }
+
+    for (let i = 0; i < n; i++) {
+      nextChecker.push(this.createCheckerBall(i, GRAY))
+    }
+
+    checkerBalls[currIdx] = newChecker
+    checkerBalls[nextIdx] = nextChecker
+    this.setState({ checkerBalls })
+  }
+
   handleMainClick = e => {
     const id = parseInt(e.target.id)
     const mainBalls = this.state.mainBalls.slice()
-    const currRow = mainBalls[this.state.currIdx]
+    const currIdx = this.state.currIdx
+    const currRow = mainBalls[currIdx]
     const isClicked = currRow[id].props.isClicked
 
     const newRow = currRow.map((ball, i) => {
@@ -91,14 +163,15 @@ class Board extends Component {
       }
     })
 
-    mainBalls[this.state.currIdx] = newRow
+    mainBalls[currIdx] = newRow
     this.setState({ mainBalls })
   }
 
   handleColorClick = e => {
     const color = e.target.style.backgroundColor
     const mainBalls = this.state.mainBalls.slice()
-    const currRow = mainBalls[this.state.currIdx]
+    const currIdx = this.state.currIdx
+    const currRow = mainBalls[currIdx]
 
     currRow.forEach((ball, i) => {
       if (ball.props.isClicked) {
@@ -106,11 +179,11 @@ class Board extends Component {
       }
     })
 
-    mainBalls[this.state.currIdx] = currRow
+    mainBalls[currIdx] = currRow
     this.setState({ mainBalls })
   }
 
-  handleNewGameClick = () => {
+  handleNewGame = _ => {
     const select = document.getElementById('select')
     const checkbox = document.getElementById('checkbox')
 
@@ -127,6 +200,27 @@ class Board extends Component {
       checkerBalls: this.setBalls(codeLen, rowLen, currIdx, false),
       answerRow: this.setAnswer(codeLen, allowDuplicates)
     })
+  }
+
+  handleCheck = _ => {
+    const mainBalls = this.state.mainBalls.slice()
+    const currIdx = this.state.currIdx
+    const currRow = mainBalls[currIdx]
+    const codeLen = this.state.codeLen
+
+    for (let i = 0; i < codeLen; i++) {
+      const currColor = currRow[i].props.color
+      if (currColor === LIGHT_GRAY) {
+        alert('Make sure there are no balls with question marks!')
+        return
+      } else if (currColor === GRAY) {
+        alert('Make sure the row is filled!')
+        return
+      }
+    }
+
+    this.updateChecker()
+    this.updateRow()
   }
 
   renderRows = () => {
@@ -172,8 +266,8 @@ class Board extends Component {
       <input id='checkbox' type='checkbox'></input>
     </div>
     <div className='button-container'>
-      <button onClick={this.handleNewGameClick}>New Game</button>
-      <button>Check</button>
+      <button onClick={this.handleNewGame}>New Game</button>
+      <button onClick={this.handleCheck}>Check</button>
     </div>
   </div>
 }
